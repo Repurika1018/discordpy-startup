9795ff76c1810c96db0a9eb7efb049d14ecd8f81
Repurika1018 <38724654+Repurika1018@users.noreply.github.com>
2020-05-11 23:30:23 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,6 +1,10 @@
 from discord.ext import commands
 import os
 import traceback
+import discord
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
