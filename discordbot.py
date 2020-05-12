5a81c3ce214f53aeee4a79d04b3c80df441d1532
Repurika1018 <38724654+Repurika1018@users.noreply.github.com>
2020-05-12 15:02:36 +0900

    ganbatta

--- discordbot.py
+++ discordbot.py
@@ -1,37 +1,27 @@
-from discord.ext import commands
-import os
-import traceback
+# インストールした discord.py を読み込む
 import discord
-import logging
 
-logging.basicConfig(level=logging.INFO)
+# 自分のBotのアクセストークンに置き換えてください
+TOKEN = 'NzA2NjExNjIzNTc3NTE4MTAy.Xro75w.fRvvW9Gfev5xb1R2YZqCtVU3gIw'
 
-bot = commands.Bot(command_prefix='/')
-token = os.environ['DISCORD_BOT_TOKEN']
+# 接続に必要なオブジェクトを生成
+client = discord.Client()
 
+# 起動時に動作する処理
+@client.event
+async def on_ready():
+    # 起動したらターミナルにログイン通知が表示される
+    print('ログインしました')
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-    
-    
-    
-    
- client = dicord.Client()
-
+# メッセージ受信時に動作する処理
 @client.event
 async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
     if message.author.bot:
         return
-    if message.content == '/neko'
-        await message.channel.send('にゃあ')
+    # 「/neko」と発言したら「にゃーん」が返る処理
+    if message.content == '/neko':
+        await message.channel.send('にゃーん')
 
-
-bot.run(token)
+# Botの起動とDiscordサーバーへの接続
+client.run(TOKEN)
