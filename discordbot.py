cd9098c690ca47b0c270678bc8f72e08833efaa7
Repurika1018 <38724654+Repurika1018@users.noreply.github.com>
2020-05-12 15:11:55 +0900

    /neko nyaa

--- discordbot.py
+++ discordbot.py
@@ -20,6 +20,11 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+    
+@bot.command()
+async def neko(ctx):
+    await ctx.send('にゃあ')
+
 
 
 bot.run(token)
